25964392b0e47f7a38fff39bb0416af8ff3c0a48
Koushic-Krus14 <117438000+Koushic-Krus14@users.noreply.github.com>
2025-07-22 18:48:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -40,6 +40,12 @@ def download():
     return send_file(file_path, as_attachment=True)
 
 
+@app.after_request
+def add_header(response):
+    response.headers['X-Frame-Options'] = 'ALLOWALL'
+    return response
+
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 10000))
     app.run(debug=True, host='0.0.0.0', port=port)
